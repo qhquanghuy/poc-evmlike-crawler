9c96305c3eba4a94d0bfe058aab4dc3eaf7e17cf
Quang Huy Nguyen <huy_ngq@septeni-technology.jp>
2021-11-04 16:13:48 +0700

    eth price stream

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import fs from 'fs'
 import pLimit from 'p-limit'
 
 import Web3 from 'web3'
@@ -13,6 +12,7 @@ import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 const dbConf = require('./database.json')
 import chains from './contracts/chains.js'
+import { map, Observable } from 'rxjs'
 
 const limit = pLimit(8)
 const abis = {
@@ -33,7 +33,21 @@ const connection = (() => {
     });
 })()
 
-const web3s = Object.fromEntries(chains.map(chain => [chain.name, new Web3(chain.provider)]))
+const web3s = Object.fromEntries(chains.map(chain => {
+    const options = {
+        // Enable auto reconnection
+        reconnect: {
+            auto: true,
+            delay: 5000, // ms
+            maxAttempts: 5,
+            onTimeout: false
+        }
+    }
+
+    const ws = new Web3.providers.WebsocketProvider(chain.provider, options)
+
+    return [chain.name, new Web3(ws)]
+}))
 
 
 function allTokens() {
@@ -50,10 +64,7 @@ function allTokens() {
 }
 
 function web3Contract(web3) {
-    return (config) => {
-
-        return new web3.eth.Contract(abis[config.abi], config.address)
-    }
+    return (config) => new web3.eth.Contract(abis[config.abi], config.address)
 }
 
 async function erc20Decimals(contract) {
@@ -106,26 +117,26 @@ async function protocolInfo() {
     return rs
 }
 
+function pairContract(web3) {
+    return (addr) => new web3.eth.Contract(abis['iuniswapv2-pair-abi'], addr)
+}
+
 async function tokenPriceIn(data) {
     const { chain, exchange, token } = data
     const web3 = web3s[chain.name]
     const contractFrom = web3Contract(web3)
     const factory = contractFrom(exchange.factory)
 
-    const price = async (token0, token1) => {
-        const pairAddr = await factory.methods.getPair(token0.address, token1.address).call()
-        const contract = new web3.eth.Contract(abis['iuniswapv2-pair-abi'], pairAddr)
-        const decimals0 = await erc20Decimals(contractFrom(token0))
-        const decimals1 =  await erc20Decimals(contractFrom(token1))
+    const price = async (tokenA, tokenB) => {
+        const pairAddr = await factory.methods.getPair(tokenA.address, tokenB.address).call()
+        const contract = pairContract(web3)(pairAddr)
+        const decimalsA = await erc20Decimals(contractFrom(tokenA))
+        const decimalsB =  await erc20Decimals(contractFrom(tokenB))
         const start = Date.now()
         const { reserve0, reserve1 } = await contract.methods.getReserves().call()
         const end = Date.now()
-
-
-        const ascOrdered = token0.address.toLowerCase() < token1.address.toLowerCase()
-
         return {
-            price: ascOrdered ? (reserve1 / reserve0) * (10**(decimals0 - decimals1)) : (reserve0 / reserve1) * (10**(decimals0 - decimals1)),
+            price: calPrice(tokenA, decimalsA, tokenB, decimalsB, reserve0, reserve1),
             start: start,
             end: end
         }
@@ -150,12 +161,69 @@ async function tokenPriceIn(data) {
 
 }
 
+function observableFromEvent(event) {
+    return new Observable(sub => {
+        event
+            .on('data', (data) => {
+                sub.next(data)
+            })
+            .on('error', (err) => {
+                sub.error(err)
+            })
+            .on('end', () => sub.unsubscribe())
+    })
+}
+
+function calPrice(tokenA, decimalsA, tokenB, decimalsB, reserve0, reserve1) {
+    const decimals = 10**(decimalsA - decimalsB)
+    const ascOrdered = tokenA.address.toLowerCase() < tokenB.address.toLowerCase()
+    return ascOrdered ? (reserve1 / reserve0) * decimals : (reserve0 / reserve1) * decimals
+}
+
+function tokenPriceStream(web3, factory) {
+    return async (tokenA, tokenB) => {
+        const decimalsB = await erc20Decimals(web3Contract(web3)(tokenA))
+        const decimalsA = await erc20Decimals(web3Contract(web3)(tokenB))
+        const pairAddr = await factory.methods.getPair(tokenA.address, tokenB.address).call()
+        const pair = pairContract(web3)(pairAddr)
+
+        const priceStream = (reserveStream, getReserve) => {
+            return reserveStream
+                .pipe(map(data => {
+                        const returnVal = data.returnValues
+                        console.log(data)
+                        const {reserve0, reserve1} = getReserve(returnVal)
+                        return calPrice(tokenA, decimalsA, tokenB, decimalsB, reserve0, reserve1)
+                    })
+                )
+        }
+
+        return {
+            sync: priceStream(observableFromEvent(pair.events.Sync()), (data) => {
+                return {
+                    reserve0: data.reserve0,
+                    reserve1: data.reserve1,
+                }
+            }),
+            swap: priceStream(observableFromEvent(pair.events.Swap()), (data) => {
+                return {
+                    reserve0: +data.amount0In === 0 ? data.amount0Out : data.amount0In,
+                    reserve1: +data.amount1Out === 0 ? data.amount1In : data.amount1Out,
+                }
+            })
+        }
+    }
+}
+
 
 async function  tokenPriceOn(chain) {
     const web3 = web3s[chain.name]
-    chain.exchanges.map(exchange => {
-
-    })
+    return Promise.all(
+        chain.exchanges.map(async (exchange) => {
+            const factory = await web3Contract(web3)(exchange.factory)
+            return tokenPriceStream(web3, factory)(exchange.eth, exchange.usdt)
+        })
+    )
 }
 
 
@@ -175,15 +243,26 @@ async function tokenPrice() {
 
 
 
-function run() {
+async function run() {
+    const obs = await tokenPriceOn(chains[2])
+    obs.map(ob => {
+        ob.swap.subscribe({
+            next(data) {
+                console.log(data)
+            },
+            error(err) { console.error('something wrong occurred: ' + err); },
+            complete() { console.log('done'); }
+        })
+    })
+
     // return tokenPrice()
-    return protocolInfo()
+    // return protocolInfo()
 }
 
 run()
-    .then(console.log)
-    .catch(console.error)
-    .finally(() => {
-        Object.values(web3s).map(web3 => web3.currentProvider.disconnect())
-        connection.destroy()
-    })
+    // .then(console.log)
+    // .catch(console.error)
+    // .finally(() => {
+    //     Object.values(web3s).map(web3 => web3.currentProvider.disconnect())
+    //     connection.destroy()
+    // })
